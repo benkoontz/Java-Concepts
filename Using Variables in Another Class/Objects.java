d306bc4575bf35dcef2a271bbe76917473a7ea77
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-12 15:07:23 -0700

    Create Objects.java

---
+++ Using Variables in Another Class/Objects.java
@@ -0,0 +1,13 @@
+package halo;
+
+public class Objects {
+	
+	public int moose = 0;
+	private int moose1 = 1;
+	protected int moose2 = 2;
+	public int year;
+	private int year1;
+	protected int year2;
+	
+
+}
