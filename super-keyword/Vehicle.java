043b22b805020114f3634535f6c92893b0d61038
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-12 15:26:25 -0700

    Create Vehicle.java

---
+++ super-keyword/Vehicle.java
@@ -0,0 +1,17 @@
+package halo;
+
+public class Vehicle {
+	
+	int maxSpeed = 120;
+	
+	Vehicle() {
+		System.out.println("vehicle constructor");
+	}
+
+	
+	void message() {
+		System.out.println("benny");
+	}
+	
+	
+}
