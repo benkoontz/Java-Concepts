11bc42132d9353f3edd0fc45e9bb655d5e6cfa3f
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 10:38:49 -0700

    Create apples.java

---
+++ Java-GUI-Mouse-Events-Program/apples.java
@@ -0,0 +1,16 @@
+package halo;
+
+import javax.swing.JFrame;
+
+// The user clicks on the window and different mouse events can happen.
+
+public class apples {
+	public static void main(String[] args) {
+	
+	Gui bucky = new Gui();
+	bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	bucky.setSize(300, 200);
+	bucky.setVisible(true);
+	
+	}
+}
