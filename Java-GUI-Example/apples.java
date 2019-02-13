5dc38f8e231b9ae609edf5700de526da452cbe87
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 10:53:35 -0700

    Create apples.java

---
+++ Java-GUI-Example/apples.java
@@ -0,0 +1,19 @@
+package hello;
+
+import javax.swing.JFrame; // gives access to using GUI
+
+// The program creates a GUI with a sentence. When the user hovers over the sentence a message shows up.
+
+public class apples {
+	
+	public static void main(String[] args) {
+		
+		tuna bucky = new tuna();
+		bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // program terminates on x button
+		bucky.setSize(275,180);
+		bucky.setVisible(true);
+		
+		
+	}
+
+}
