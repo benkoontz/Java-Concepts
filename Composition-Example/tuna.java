e104f9a7e73a957db793c3f9f92b05f17a64cd1a
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 10:57:13 -0700

    Create tuna.java

---
+++ Composition-Example/tuna.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class tuna {
+	private String name;
+	private potpie birthday; // reference to potpie object
+	
+	public tuna(String theName, potpie theDate) {		// constructor
+		name = theName;
+		birthday = theDate; 
+	}
+	
+	public String toString() {
+		return String.format("My name is %s, my birthday is %s", name, birthday);
+	}
+}
