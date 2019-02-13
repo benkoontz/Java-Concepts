5ebaa3bfcb6542de45fd193a835ee622ccf8d610
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 10:32:21 -0700

    Create apples.java

---
+++ Java-GUI-FlowLayout/apples.java
@@ -0,0 +1,20 @@
+package halo;
+
+import java.util.*;
+
+import javax.swing.JFrame;
+
+// program for flow layout
+
+public class apples {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		Layout l = new Layout();
+		l.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		l.setSize(300, 100);
+		l.setVisible(true);
+	
+	
+	}
+}
