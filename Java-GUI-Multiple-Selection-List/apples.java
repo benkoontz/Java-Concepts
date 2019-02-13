b8be41a810c1bcba96bd1f4e04bc2032e7b33a74
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 10:40:44 -0700

    Create apples.java

---
+++ Java-GUI-Multiple-Selection-List/apples.java
@@ -0,0 +1,17 @@
+package halo;
+
+import javax.swing.JFrame;
+
+// The user clicks on different values in a list and they can moves 
+// those values to another list. The user can select more than one item from the list.
+
+public class apples {
+	public static void main(String[] args) {
+	
+	Gui bucky = new Gui();
+	bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	bucky.setSize(300, 200);
+	bucky.setVisible(true);
+	
+	}
+}
