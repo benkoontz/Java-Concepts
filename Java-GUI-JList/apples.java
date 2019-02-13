a6138b6244cce4826869bb73032aa7e0f9d8094f
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 10:42:13 -0700

    Create apples.java

---
+++ Java-GUI-JList/apples.java
@@ -0,0 +1,16 @@
+package halo;
+
+import javax.swing.JFrame;
+
+// The user clicks on different items on the list and the background color changes.
+
+public class apples {
+	public static void main(String[] args) {
+	
+	Gui bucky = new Gui();
+	bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	bucky.setSize(300, 200);
+	bucky.setVisible(true);
+	
+	}
+}
