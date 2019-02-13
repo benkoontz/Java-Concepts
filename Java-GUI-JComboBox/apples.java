f03df27a61bcf448ea33ae238bea6aeb4245d8a2
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 10:43:38 -0700

    Create apples.java

---
+++ Java-GUI-JComboBox/apples.java
@@ -0,0 +1,17 @@
+package halo;
+
+import javax.swing.JFrame;
+
+// The user clicks on a combo box and can select between different pictures.
+
+
+public class apples {
+	public static void main(String[] args) {
+	
+	Gui bucky = new Gui();
+	bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	bucky.setSize(300, 200);
+	bucky.setVisible(true);
+	
+	}
+}
