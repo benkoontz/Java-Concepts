62054e3c4559e15dca44e4400ccd907d67f19cd1
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 10:52:15 -0700

    Create apples.java

---
+++ Java-GUI-Event-Handler/apples.java
@@ -0,0 +1,18 @@
+package hello;
+
+import javax.swing.JFrame;
+
+// The program creates a window that has four fields. The fields are a blank text field, a text field with text, an uneditable text field,
+// and a password text field. When the user presses enter on each field, information from each field shows up.
+
+
+public class apples {
+	public static void main(String[] args) {
+	
+	tuna bucky = new tuna();
+	bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	bucky.setSize(350, 100);
+	bucky.setVisible(true);
+	
+	}
+}
