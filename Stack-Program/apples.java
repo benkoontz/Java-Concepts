accd1ab1142ad83d8be58dd1ecb72e8d602286f8
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 10:21:45 -0700

    Create apples.java

---
+++ Stack-Program/apples.java
@@ -0,0 +1,35 @@
+package halo;
+
+import java.util.*;
+
+// Stacks are closely related to a stack of dishes.
+
+public class apples {
+	public static void main(String[] args) {
+		
+		Stack<String> stack = new Stack<String>();
+		stack.push("bottom");
+		printStack(stack);
+		stack.push("second");
+		printStack(stack);
+		stack.push("third");
+		printStack(stack);
+		
+		stack.pop();
+		printStack(stack);
+		stack.pop();
+		printStack(stack);
+		stack.pop();
+		printStack(stack);
+		
+		
+	}
+	
+	private static void printStack(Stack<String> s) {
+		if(s.isEmpty())
+			System.out.println("you have nothing in your stack");
+		else
+			System.out.printf("%s top\n", s);
+	}
+	
+}
