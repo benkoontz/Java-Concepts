50562fab66bfcd2458feafe61ffc51b166f2c38b
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 10:45:41 -0700

    Create apples.java

---
+++ Java-GUI-JRadioButton/apples.java
@@ -0,0 +1,16 @@
+package halo;
+
+import javax.swing.JFrame;
+
+// The user can type in a text box and select radio buttons to change the style of the text
+
+public class apples {
+	public static void main(String[] args) {
+	
+	Gui bucky = new Gui();
+	bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	bucky.setSize(300, 200);
+	bucky.setVisible(true);
+	
+	}
+}
