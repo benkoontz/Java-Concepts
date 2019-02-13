a191a140ac0fd0ff5e8398f0e46fd89f227174c1
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 10:46:59 -0700

    Create apples.java

---
+++ Java-GUI-Check-Box/apples.java
@@ -0,0 +1,17 @@
+package halo;
+
+import javax.swing.JFrame;
+
+// A user types into a text field.
+// The user can make the text in the text field change by clicking either the bold check box or the italic check box.
+
+public class apples {
+	public static void main(String[] args) {
+	
+	Gui bucky = new Gui();
+	bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	bucky.setSize(300, 200);
+	bucky.setVisible(true);
+	
+	}
+}
