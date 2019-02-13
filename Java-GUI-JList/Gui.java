f838073e84e59b5b4fdfb91999e74baf2e392604
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 10:42:35 -0700

    Create Gui.java

---
+++ Java-GUI-JList/Gui.java
@@ -0,0 +1,39 @@
+package halo;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+// JList program
+
+
+public class Gui  extends JFrame { // JFrame gives all the basic windows features (minmize button, X button, etc)
+	
+	private JList list;
+	private static String[] colornames = {"black", "blue", "red", "white"};
+	private static Color[] colors = {Color.BLACK, Color.BLUE, Color.RED, Color.WHITE};
+	
+	public Gui () {
+		super("title");
+		setLayout(new FlowLayout());
+		
+		list = new JList(colornames);
+		list.setVisibleRowCount(4);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		add(new JScrollPane(list));
+		
+		list.addListSelectionListener(
+				new ListSelectionListener() {
+					public void valueChanged(ListSelectionEvent event) {
+						getContentPane().setBackground(colors[list.getSelectedIndex()]);
+					}
+				}
+				
+				
+			);
+	}
+	
+
+	
+}
