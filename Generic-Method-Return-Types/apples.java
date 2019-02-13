d417ea66d7bd6b1b2bb94f16373ca0eb85ad0508
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-12 20:45:52 -0700

    Create apples.java

---
+++ Generic-Method-Return-Types/apples.java
@@ -0,0 +1,27 @@
+package halo;
+
+import java.util.*;
+
+// Generic method that has a return type
+
+public class apples {
+	public static void main(String[] args) {
+		
+		System.out.println(max(23, 42, 1));
+		System.out.println(max("apples","tots", "chicken"));
+	}
+	
+	
+	// only objects from the Comparable class can be used in this method
+	public static <T extends Comparable<T>>  T max(T a, T b, T c) { 
+		T m = a;
+		
+		if(b.compareTo(a) > 0)
+			m = b;
+		
+		if(c.compareTo(m) > 0)
+			m = c;
+		
+		return m;
+	}
+}
